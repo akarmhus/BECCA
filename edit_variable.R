f11d9ec9a55c6b8217b417192af322d790c63cce
akarmhus <andreas.aaka@gmail.com>
2016-11-10 10:24:11 +0100

    Add files via upload

---
+++ edit_variable.R
@@ -0,0 +1,9 @@
+edit.var <- function(x, settings) {
+  
+  # Function to edit variables in new uploaded dataset at user command.
+  
+  # x <- name as variable to edit as character string.
+  # settings <- a list containing new settings for x.
+  
+  
+}
